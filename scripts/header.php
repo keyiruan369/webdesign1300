75906fdbbabdb914ee9b26eecdc635ec399325f0
Keyi Ruan <kathyruan@Kathys-MacBook-Pro.local>
2016-11-28 01:17:19 -0500

    php fixed

--- scripts/header.php
+++ scripts/header.php
@@ -1,19 +1,24 @@
-<?php
-    echo '
-    <div id="title">
-        <h1> Ithaca Apple Harvest Festival </h1>
-    </div>
+ <head>
+         <meta charset="utf-8">
+         <title> Ithaca Apple Harvest Festival </title>
+         <link rel="stylesheet" type="text/css" href="styles/main.css">
+         <link rel="stylesheet" type="text/css" href="styles/headerfooter.css">
+         <link href='http://fonts.googleapis.com/css?family=Playfair+Display:400,700,900,400italic,700italic,900italic|Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
 
-    <div id="container">
-        <div id="nav">
-            <ul>
-                <li><a href="index.php"><strong>Home</strong></a></li>
-                <li><a href="map.php"><strong>Map</strong></a></li>
-                <li><a href="gallery.php"><strong>Gallery</strong></a></li>
-                <li><a href="calendar.php"><strong>Calendar</strong></a></li>
-                <li><a href="must-see.php"><strong>Must See</strong></a></li>
-            </ul>
-        </div>
+         <div id="title">
+           <h1> Ithaca Apple Harvest Festival </h1>
+         </div>
+
+         <div id="container">
+            <div id="nav">
+              <ul>
+                 <li><a href="index.php"><strong>Home</strong></a></li>
+                 <li><a href="map.php"><strong>Map</strong></a></li>
+                 <li><a href="gallery.php"><strong>Gallery</strong></a></li>
+                 <li><a href="calendar.php"><strong>Calendar</strong></a></li>
+                 <li><a href="must-see.php"><strong>Must See</strong></a></li>
+              </ul>
+            </div>
 
         <div id="banner_photos">
             <div id="photo_1">
@@ -36,7 +41,4 @@
             </div>
         </div>
     </div>
-
-    '
-    ;
-?>
+ </head>
