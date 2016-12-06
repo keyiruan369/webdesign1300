3411927f81dcc04cc6dc5fbd53f616a7a757dce2
Jennifer Kauffman <Jennifer Kauffman>
2016-12-05 22:57:31 -0500

    Updates including APA citations

--- calendar.php
+++ calendar.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
-
 <html lang="en"> 
-    <head>
-         <meta charset="utf-8">
-         <title> Ithaca Apple Harvest Festival </title>
-         <link rel="stylesheet" type="text/css" href="styles/main.css">
-         <link rel="stylesheet" type="text/css" href="styles/headerfooter.css">
-         <link href="http://fonts.googleapis.com/css?family=Playfair+Display:400,700,900,400italic,700italic,900italic%7CDroid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css">
-    </head>
-       <body>
-       <?php include "includes/header.php";?>
-       <?php include "includes/footer.php"; ?>
-    </body>
-    
-</html>	
+  <head>
+    <meta charset="utf-8">
+    <title> Ithaca Apple Harvest Festival </title>
+    <link rel="stylesheet" type="text/css" href="styles/main.css">
+    <link rel="stylesheet" type="text/css" href="styles/headerfooter.css">
+    <link href="http://fonts.googleapis.com/css?family=Playfair+Display:400,700,900,400italic,700italic,900italic%7CDroid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <?php include "includes/header.php";?>
+
+    <!--This image was made by Jennifer Kauffman -->
+    <img src="images/calendar.png" alt="Schedule of activities">
+
+    <?php include "includes/footer.php"; ?>
+  </body>  
+</html> 
--- index.php
+++ index.php
@@ -1,86 +1,100 @@
 <!DOCTYPE html>
-
 <html lang="en">
-    <head>
-         <meta charset="utf-8">
-         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-         <title> Ithaca Apple Harvest Festival </title>
-         <link rel="stylesheet" type="text/css" href="styles/main.css">
-         <link rel="stylesheet" type="text/css" href="styles/headerfooter.css">
-         <link href="http://fonts.googleapis.com/css?family=Playfair+Display:400,700,900,400italic,700italic,900italic%7CDroid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css">
-    </head>
-
-    <body>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Ithaca Apple Harvest Festival </title>
+    <link rel="stylesheet" type="text/css" href="styles/main.css">
+    <link rel="stylesheet" type="text/css" href="styles/headerfooter.css">
+    <link href="http://fonts.googleapis.com/css?family=Playfair+Display:400,700,900,400italic,700italic,900italic%7CDroid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+  </head>
+  <body>
     <div id="wrapper">
-    <?php include "includes/header.php";?>
-        <div id="homepage_container">
-            <div id="coupons">
-                <h3>Coupons</h3>
-                <!--This image was made by Colby Triolo -->
-                <img class="coupon" src="images/coupon.jpg" alt="coupon place holder">
-                <img class="coupon" src="images/coupon.jpg" alt="coupon place holder">
-                <img class="coupon" src="images/coupon.jpg" alt="coupon place holder">
-                <img class="coupon" src="images/coupon.jpg" alt="coupon place holder">
-                <img class="coupon" src="images/coupon.jpg" alt="coupon place holder">
-                <img class="coupon" src="images/coupon.jpg" alt="coupon place holder">
-            </div>
-            <div id="right">
-                <div id="about">
-                    <h2> Ithaca's premier event of the Fall,</h2><h3> Apple Harvest Festival is a three
-                     day celebration of food, fun, and apples!</h3>
-                    <p><strong> by: Downtown Ithaca </strong></p>
-                    <p> The 34th Annual Great Downtown Ithaca Apple Harvest Festival by 
-                    Presented Tompkins Trust Company is set to take place September 30 - 
-                    October 2, 2016.  The 2016 Apple Harvest Festival hosts plenty of apples, 
-                    local produce, fresh baked goods, family entertainment at every turn, games,
-                     rides, prizes and live performances on two stages. Apple Harvest Festival 
-                     is a great introduction to the rich farm and artist community of Ithaca.  
-                     First time visitors can witness a variety of wonderful local entertainment
-                      while also visiting the historical outdoor pedestrian shopping mall, The 
-                      Ithaca Commons.  The pedestrian mall is the epicenter of the festival and
-                    the gateway to a nostalgic journey to harvest season fun.</p>
-                    <!-- Image from: https://www.flickr.com/photos/106103661@N02/21406193723/in/album-72157657274645113/ -->
-                    <img src="images/apples.jpg" alt="apples galore">
-                    <p>Over 100 vendors will be selling unique apple varieties, hot apple cider
-                     donuts, kettle corn, local farm fresh baked goods and a multitude of around
-                     the world specialties. Local farmers are on site selling everything from tasty
-                      apple beverages to local pumpkins and of course, apples, apples and more 
-                      apples. Wineries and cideries have regional beverages available for tasting 
-                      and purchase.  Kids can visit with the goats and learn how fiber is made with 
-                      Laughing Goat Fiber Farm.  Looking for quality goods, visit 60 different
-                       handcrafting artisans along Cayuga Street at the fall craft show which includes
-                       jewelry, ceramics, home decor, body care, specialty gifts, unique sculpture, clothing and 
-                       so much more. </p>
-
-                </div>
-                <div id="restaurants">
-                    <h3> Where To Eat</h3>
-                    <div id="logos">
-                        <!--Edited from https://www.facebook.com/collegetownbagels/photos/a.499821170999.308662.113060610999/10153914872621000/?type=1&theater -->
-                        <a href="http://www.collegetownbagels.com"  target="_blank">
-                        <img class="restaurant" src="images/CTB.jpg" alt="CTB"></a>
-                        <!--Edited from https://www.facebook.com/543285565850581/photos/a.543286069183864.1073741825.543285565850581/543286075850530/?type=1&theater -->
-                        <a href="http://www.maxsithaca.com" target="_blank">
-                        <img class="restaurant" src="images/maxs.jpg" alt="Max's Restaurant"></a>
-                        <!-- Edited from https://www.facebook.com/Lot10Ithaca/photos/a.160007510774359.31657.160005544107889/199416176833492/?type=1&theater -->
-                        <a href="http://www.lot-10.com" target="_blank">
-                        <img class="restaurant" src="images/lot10.jpg" alt="Lot 10"></a>
-                        <!-- Edited from https://www.facebook.com/MiaIthaca/photos/a.227824170572748.58461.227824003906098/230272143661284/?type=1&theater -->
-                        <a href="http://www.miaithaca.com" target="_blank">
-                        <img class="restaurant" src="images/mias.jpg" alt="Mia's Tapas"></a>
-                        <!--Edited from: https://www.facebook.com/121835437827171/photos/a.182220345122013.46947.121835437827171/182220348455346/?type=1&theater -->
-                        <a href="http://www.vivataqueria.com/" target="_blank">
-                        <img class="restaurant" src="images/viva.jpg" alt="Viva!"></a>
-                        <!-- Edited from https://www.facebook.com/mahoganygrill/photos/a.115090538552781.14698.115088588552976/115090541886114/?type=1&theater -->
-                        <a href="http://www.mahoganygrill.com" target="_blank">
-                        <img class="restaurant" src="images/mg.jpg" alt="Mahogany Grill"> </a>
-                        </div>
-                </div>
-            </div>
+      <?php include "includes/header.php";?>
+      <div id="homepage_container">
+        <div id="coupons">
+          <h3>Coupons</h3>
+          <!--This image was made by Colby Triolo -->
+          <img class="coupon" src="images/coupon.jpg" alt="coupon place holder">
+          <img class="coupon" src="images/coupon.jpg" alt="coupon place holder">
+          <img class="coupon" src="images/coupon.jpg" alt="coupon place holder">
+          <img class="coupon" src="images/coupon.jpg" alt="coupon place holder">
+          <img class="coupon" src="images/coupon.jpg" alt="coupon place holder">
+          <img class="coupon" src="images/coupon.jpg" alt="coupon place holder">
         </div>
-        <?php include "includes/footer.php"; ?>
-        </div>
-    </body>
+        <div id="right">
+          <div id="about">
+            <h2> Ithaca's premier event of the Fall,</h2>
+            <h3> Apple Harvest Festival is a three day celebration of food, fun, and apples!</h3>
+            <p><strong> by: Downtown Ithaca </strong></p>
+            <p> The 34th Annual Great Downtown Ithaca Apple Harvest Festival by 
+            Presented Tompkins Trust Company is set to take place September 30 - 
+            October 2, 2016.  The 2016 Apple Harvest Festival hosts plenty of apples, 
+            local produce, fresh baked goods, family entertainment at every turn, games,
+            rides, prizes and live performances on two stages. Apple Harvest Festival 
+            is a great introduction to the rich farm and artist community of Ithaca.  
+            First time visitors can witness a variety of wonderful local entertainment
+            while also visiting the historical outdoor pedestrian shopping mall, The 
+            Ithaca Commons.  The pedestrian mall is the epicenter of the festival and
+            the gateway to a nostalgic journey to harvest season fun.</p>
+            <!-- This image belongs to Andrew Halpern:
+            Halpern, A. (2015, October 3). Retrieved from https://www.flickr.com/photos/106103661@N02/21406193723/in/album-72157657274645113/ 
+            -->
+            <img src="images/apples.jpg" alt="apples galore">
+            <p>Over 100 vendors will be selling unique apple varieties, hot apple cider
+            donuts, kettle corn, local farm fresh baked goods and a multitude of around
+            the world specialties. Local farmers are on site selling everything from tasty
+            apple beverages to local pumpkins and of course, apples, apples and more 
+            apples. Wineries and cideries have regional beverages available for tasting 
+            and purchase.  Kids can visit with the goats and learn how fiber is made with 
+            Laughing Goat Fiber Farm.  Looking for quality goods, visit 60 different
+            handcrafting artisans along Cayuga Street at the fall craft show which includes
+            jewelry, ceramics, home decor, body care, specialty gifts, unique sculpture, clothing and 
+            so much more. </p>
+          </div>
+          <div id="restaurants">
+            <h3> Where To Eat</h3>
+            <div id="logos">
+              <!-- This image belongs to Collegetown Bagels:
+              Collegetown Bagels. (2016, February 25). Retrieved from https://www.facebook.com/collegetownbagels/photos/a.499821170999.308662.113060610999/10153914872621000/?type=1&theater
+              -->
+              <a href="http://www.collegetownbagels.com"  target="_blank">
+              <img class="restaurant" src="images/CTB.jpg" alt="CTB"></a>
 
-    
+              <!-- This image belongs to Max's Classic American Grill-Ithaca:
+              Max's Classic American Grill-Ithaca. (2016, February 29). Retrieved from https://www.facebook.com/543285565850581/photos/a.543286069183864.1073741825.543285565850581/543286075850530/?type=1&theater
+              -->
+              <a href="http://www.maxsithaca.com" target="_blank">
+              <img class="restaurant" src="images/maxs.jpg" alt="Max's Restaurant"></a>
+
+              <!-- This image belongs to Lot 10:  
+              Lot 10. (2012, March 20). Retrieved from https://www.facebook.com/Lot10Ithaca/photos/a.160007510774359.31657.160005544107889/199416176833492/?type=1&theater
+              -->
+              <a href="http://www.lot-10.com" target="_blank">
+              <img class="restaurant" src="images/lot10.jpg" alt="Lot 10"></a>
+                        
+              <!-- This image belongs to Mia Restaurant:
+              Mia Restaurant. (2011, July 4). Retrieved from https://www.facebook.com/MiaIthaca/photos/a.227824170572748.58461.227824003906098/230272143661284/?type=1&theater
+              -->
+              <a href="http://www.miaithaca.com" target="_blank">
+              <img class="restaurant" src="images/mias.jpg" alt="Mia's Tapas"></a>
+                        
+              <!-- This image belongs to Viva Taqueria:
+              Viva Taqueria. (2010, December 6). Retrieved from https://www.facebook.com/121835437827171/photos/a.182220345122013.46947.121835437827171/182220348455346/?type=1&theater
+              -->
+              <a href="http://www.vivataqueria.com/" target="_blank">
+              <img class="restaurant" src="images/viva.jpg" alt="Viva!"></a>
+                        
+              <!-- This image belongs to Mahogany Grill:
+              Mahogany Grill. (2010, October 10). Retrieved from https://www.facebook.com/mahoganygrill/photos/a.115090538552781.14698.115088588552976/115090541886114/?type=1&theater
+              -->
+              <a href="http://www.mahoganygrill.com" target="_blank">
+              <img class="restaurant" src="images/mg.jpg" alt="Mahogany Grill"> </a>
+              </div>
+            </div>
+          </div>
+      </div>
+      <?php include "includes/footer.php"; ?>
+      </div>
+  </body>
 </html>
