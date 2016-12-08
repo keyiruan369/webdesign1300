e48dabec71cfd241321e7eb0951a3ffb392c5c35
Keyi Ruan <kathyruan@Kathys-MacBook-Pro.local>
2016-12-08 06:47:00 -0500

    fixed footer css for mobile

--- map.php
+++ map.php
@@ -2,19 +2,41 @@
 <html lang="en"> 
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title> Ithaca Apple Harvest Festival </title>
       <link rel="stylesheet" type="text/css" href="styles/main.css">
       <link rel="stylesheet" type="text/css" href="styles/headerfooter.css">
       <link href="http://fonts.googleapis.com/css?family=Playfair+Display:400,700,900,400italic,700italic,900italic%7CDroid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+      <script src="js/map.js"></script>
   </head>
   <body>
+    <div id="wrapper">
     <?php include "includes/header.php";?>
-    <div id="map">
-      <!-- This image belongs to Downtown Ithaca:
-      Downtown Ithaca. (n.d.). Retrieved from http://www.downtownithaca.com/local/file_upload/images/Apple Harvest Map 2016.jpg
-      -->
-      <img src="images/map.jpg" alt="map">  
-    </div>  
+    <div id="map_container">
+      <div id="map">
+         <h1>Apple Harvest Festival Map</h1>
+         <!-- This image belongs to Downtown Ithaca:
+         Downtown Ithaca. (n.d.). Retrieved from http://www.downtownithaca.com/local/file_upload/images/Apple Harvest Map 2016.jpg -->
+         <img src="images/applemap.jpg" alt="map">  
+      </div>
+      <div id="bus">  
+          <h1>Bus Routes</h1>
+          <div id="button_div">
+              <button id="button1" onclick="image_slide(1);">Previous Route</button>
+              <button id="button2" onclick="image_slide(0);">Next Route</button>
+          </div> <!-- end of button_div -->
+          <img id="default_bus" src="images/bus1.jpg" alt="bus1">
+          <!-- bus1.jpg belongs to TCAT:
+          TCAT. (n.d.). Retrieved from https://tcat.nextinsight.com/mrnpics/10_Jan2015.jpg -->
+          <!-- bus2.jpg belongs to TCAT:
+          TCAT. (n.d.). Retrieved from https://tcat.nextinsight.com/mrnpics/30_August2016.2.jpg -->
+          <!-- bus3.jpg belongs to TCAT:
+          TCAT. (n.d.). Retrieved from https://tcat.nextinsight.com/mrnpics/17_Summer2015.jpg -->
+          <!-- bus4.jpg belongs to TCAT:
+          TCAT. (n.d.). Retrieved from https://tcat.nextinsight.com/mrnpics/11_August2016_SNOW.jpg -->
+      </div>  
+    </div>
     <?php include "includes/footer.php"; ?>
+  </div>
   </body>  
 </html> 
--- must-see.php
+++ must-see.php
@@ -3,14 +3,64 @@
 <html lang="en"> 
     <head>
          <meta charset="utf-8">
+         <meta name="viewport" content="width=device-width, initial-scale=1.0">
          <title> Ithaca Apple Harvest Festival </title>
          <link rel="stylesheet" type="text/css" href="styles/main.css">
          <link rel="stylesheet" type="text/css" href="styles/headerfooter.css">
          <link href="http://fonts.googleapis.com/css?family=Playfair+Display:400,700,900,400italic,700italic,900italic%7CDroid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css">
     </head>
        <body>
+       <div id="wrapper">
        <?php include "includes/header.php";?>
+       <div id="mustsee_container">
+           <div id="juggleboy">
+             
+              <img src="images/juggleboy.jpg" alt="juggleboy">
+              <!-- This image belongs to Michael Hilbig:
+              Hilby The Skinny German Juggle Boy. (n.d.). Retrieved from https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/1237637_626331887405109_2098819015_n.jpg?
+              oh=aff83fe6d405481b26f88163a3f270b8&oe=58C33E4B -->
+              <h2> Hilby The Skinny German Juggle Boy</h2>
+              <h3> Time: September 30 Friday 12:30-4:45PM </h3>
+              <h3> Location: West State Street </h3>
+              <p> Michael Hilbig, aka, Hilby the Skinny German Juggle boy is a comedic circus stunt performer from Ithaca, NY. One 
+                cannot help but be charmed by Hilby’s impeccable timing and extreme circus skills. You will find Hilby on top of 
+                Unicycles of all shapes and sizes manipulating objects ranging from potato chips to electrical powered hedge trimmers.
+                 His show is a powerful and spell binding collage of superb technical skill and wacky comedy in the tradition of vaudeville.
+              </p>
+           </div> 
+           <div id="comedy">             
+              <img src="images/comedy.jpg" alt="comedy">
+              <!-- This image belongs to Comedy Soap Box:
+              Comedy Soap Box. (n.d.). Retrieved from hhttp://www.comedysoapbox.com/festivals.cfm?ID=158 -->
+              <h2> Fingers Lake Comedy Festival</h2>
+              <div id="Fri">
+              <h3> Friday September 30th</h3>
+              <p>6pm -FLCF Competition 1st Round @ Lot 10(Age 21+)</p>
+              <p>8pm -Stand-up Comedy Show @Acting Out NY(AONY)</p>
+              <p>10pm -All-Star Comedy Show @ ANOY</p>
+              </div>
+              <div id="Sat">
+              <h3>Saturday October 1st</h3>
+              <p>6pm -Stand-Up Comedy Show @ Lot 10(Age 21+)</p>
+              <p>8pm -FLCF Competition Final Round @AONY</p>
+              <p>10pm -All-Star Comedy Show @ ANOY</p>
+              </div>
+           </div> 
+           <div id="band">
+              <img src="images/band.jpg" alt="band">
+              <!-- This image belongs to Michael Hilbig:
+              Hilby The Skinny German Juggle Boy. (n.d.). Retrieved from https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/1237637_626331887405109_2098819015_n.jpg?
+              oh=aff83fe6d405481b26f88163a3f270b8&oe=58C33E4B -->
+              <h2> Wailin' Brass Band</h2>
+              <h3> Time: October 1 Saturday 5:00-6:00PM </h3>
+              <h3> Location: Bernie Milton Pavilion</h3>
+              <p> Wailin' Brass Band is based in Ithaca, NY. Come check out Wailin' Brass Band on the Bernie Milton Pavilion Stage at the Ithaca
+               Apple Harvest Festival! They will be playing sweet tunes ranging from New Orleans Traditionals to pop!
+              </p>
+           </div> 
+       </div>
        <?php include "includes/footer.php"; ?>
+     </div>
     </body>
     
 </html>	
